22b93e074d6bb8bb391107292b1d5868740c46d8
Hanashi <hanashi@hanashi.eu>
2019-03-14 18:32:09 +0100

    Exception abgefangen

--- files/lib/system/cache/builder/TeamSpeakViewerMenuBuilder.class.php
+++ files/lib/system/cache/builder/TeamSpeakViewerMenuBuilder.class.php
@@ -15,26 +15,33 @@ class TeamSpeakViewerMenuBuilder extends AbstractCacheBuilder {
 	 * @inheritDoc
 	 */
     protected function rebuild(array $parameters) {
-        $clientlist = TeamSpeakViewerHandler::getInstance()->clientlist(['-away', '-voice', '-groups']);
-        $clientlist = TeamSpeakUtil::arraySort($clientlist, 'client_nickname');
+        try {
+            $clientlist = TeamSpeakViewerHandler::getInstance()->clientlist(['-away', '-voice', '-groups']);
+            $clientlist = TeamSpeakUtil::arraySort($clientlist, 'client_nickname');
 
-        $channellist = TeamSpeakViewerHandler::getInstance()->channellist();
-        $channellistTmp = [];
-        foreach ($channellist as $channel) {
-            $channellistTmp[$channel['cid']] = $channel;
-        }
-
-        $clientlistTmp = [];
-        foreach ($clientlist as $client) {
-            if ($client['client_type'] == 1 && !HANASHI_TEAMSPEAK_VIEWER_SHOW_QUERY) continue;
-
-            $client['channel'] = 'Unknown';
-            if (isset($channellistTmp[$client['cid']])) {
-                $client['channel'] = $channellistTmp[$client['cid']]['channel_name'];
+            $channellist = TeamSpeakViewerHandler::getInstance()->channellist();
+            $channellistTmp = [];
+            foreach ($channellist as $channel) {
+                $channellistTmp[$channel['cid']] = $channel;
             }
-            $clientlistTmp[] = $client;
-        }
 
-        return $clientlistTmp;
+            $clientlistTmp = [];
+            foreach ($clientlist as $client) {
+                if ($client['client_type'] == 1 && !HANASHI_TEAMSPEAK_VIEWER_SHOW_QUERY) continue;
+
+                $client['channel'] = 'Unknown';
+                if (isset($channellistTmp[$client['cid']])) {
+                    $client['channel'] = $channellistTmp[$client['cid']]['channel_name'];
+                }
+                $clientlistTmp[] = $client;
+            }
+
+            return $clientlistTmp;
+        } catch (TeamSpeakException $e) {
+            if (ENABLE_DEBUG_MODE) {
+				throw $e;
+			}
+            return [];
+        }
     }
 }
