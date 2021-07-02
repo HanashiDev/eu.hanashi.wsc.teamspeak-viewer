7f52a159d6652144469664252b22c61179669602
Peter Lohse <github@hsrvmail.net>
2021-07-02 08:26:34 +0200

    Update auf 1.0.2

--- make.sh
+++ make.sh
@@ -5,3 +5,5 @@ rm -f templates.tar
 7z a -ttar -mx=9 templates.tar ./templates/*
 rm -f eu.hanashi.wsc.teamspeak-viewer.tar
 7z a -ttar -mx=9 eu.hanashi.wsc.teamspeak-viewer.tar ./* -x!acptemplates -x!files -x!templates -x!eu.hanashi.wsc.teamspeak-viewer.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
+rm -f files.tar
+rm -f templates.tar
