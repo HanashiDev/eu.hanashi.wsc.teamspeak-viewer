fee085e71bbf0d32ddcd299ba89572293bf72cdf
Peter Lohse <github@hsrvmail.net>
2021-08-16 23:00:34 +0200

    update build script

--- make.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-del files.tar
-7z a -ttar -mx=9 files.tar .\files\*
-del templates.tar
-7z a -ttar -mx=9 templates.tar .\templates\*
-del eu.hanashi.wsc.teamspeak-viewer.tar
-7z a -ttar -mx=9 eu.hanashi.wsc.teamspeak-viewer.tar .\* -x!acptemplates -x!files -x!templates -x!eu.hanashi.wsc.teamspeak-viewer.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
